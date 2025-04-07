6ab7b71d532c1206a0131d89ff4fe8cc0474fa7b
Tarun-kondaveti <tarun.kondaveti@gmail.com>
2025-04-07 09:42:19 +0530

    added addtion logic

--- helloo.java
+++ helloo.java
@@ -1,5 +1,9 @@
 public class helloo {
     public static void main(String args[]){
         System.out.println("Today is Devops Lab Internal ");
+        int a =10;
+        int b=20;
+        int c=a+b;
+        System.out.println("Addition of 10 and 20 is : "+c);
     }
 }
